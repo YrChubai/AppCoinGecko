f61b1e526a5224ba86e0ae8c9163f590a1567942
YrChubai <razawchs@gmail.com>
2024-08-21 23:39:55 +0300

    Add Color and Edit Theme

--- app/src/main/java/com/example/appcoingecko/ui/theme/Theme.kt
+++ app/src/main/java/com/example/appcoingecko/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DominantOrange,
+    secondary = SecondOrange,
+    tertiary = CryptoGray
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = DominantOrange,
+    secondary = SecondOrange,
+    tertiary = CryptoGray
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
