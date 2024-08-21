683d13093781cf19857a4c8abdb68693856ef9f9
YrChubai <razawchs@gmail.com>
2024-08-21 23:34:45 +0300

    Add Font Roboto

--- app/src/main/java/com/example/appcoingecko/ui/theme/Type.kt
+++ app/src/main/java/com/example/appcoingecko/ui/theme/Type.kt
@@ -2,14 +2,23 @@ package com.example.appcoingecko.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.appcoingecko.R
 
 // Set of Material typography styles to start with
+val Roboto = FontFamily(
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
