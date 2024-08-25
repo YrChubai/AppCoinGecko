14a92eacfcdb6a2899aefe90db12d2f01a2b8c71
YrChubai <razawchs@gmail.com>
2024-08-25 22:33:46 +0300

    Add CryptoDetailViewModel

---
+++ app/src/main/java/com/example/appcoingecko/ui/screen/crypto_detail/CryptoDetailViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.traineapp.ui.screen.crypto_detail
+
+import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.appcoingecko.data.repository.CryptoRepository
+import com.example.traineapp.data.model.CoinCryptoDetail
+import com.example.traineapp.data.model.CoinCryptoDetailModif
+import com.example.traineapp.data.remote.AdapterApi
+
+import kotlinx.coroutines.launch
+
+class CryptoDetailViewModel : ViewModel(){
+    private val apiService = AdapterApi.create()
+    private val repository = CryptoRepository(apiService)
+
+    private val _state = mutableStateOf(CryptoDetailState())
+    val state: State<CryptoDetailState> = _state
+
+    fun loadItems(id: String) {
+        viewModelScope.launch {
+            try {
+                _state.value = CryptoDetailState(isLoading = true)
+
+                val call = repository.fetchItemDetail(id)
+                val result = formateRespone(call)
+
+                _state.value = CryptoDetailState(coin = result)
+                Log.d("Loadeddetail", _state.value.toString())
+
+            } catch (e: Exception){
+                _state.value = CryptoDetailState(
+                    error = e.message ?: "An unexpected error occured"
+                )
+                Log.d("ERRORdetail", e.toString())
+            }
+        }
+    }
+    fun formateRespone(responsetItem: CoinCryptoDetail) : CoinCryptoDetailModif{
+
+        val cleanedString = responsetItem.description["en"]!!.replace("\r\n", "\n")
+
+        // Remove <a> tags and their content
+        val aTagPattern = Regex("<a[^>]*>.</a>", RegexOption.DOT_MATCHES_ALL)
+        val withoutATags = cleanedString.replace(aTagPattern, "")
+
+        // Remove remaining HTML tags
+        val htmlPattern = Regex("<[^>]*>")
+        val withoutHtmlTags = withoutATags.replace(htmlPattern, "")
+
+        // Remove URLs
+        val urlPattern = Regex("https?://\\S+")
+        val finalText = withoutHtmlTags.replace(urlPattern, "")
+
+        val urlString = responsetItem.image["large"]!!
+
+        val announcementsModif =
+                CoinCryptoDetailModif(
+                    responsetItem.id,
+                    responsetItem.name,
+                    finalText,
+                    responsetItem.categories,
+                    urlString
+                )
+        return announcementsModif
+    }
+
+}
